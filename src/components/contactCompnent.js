ea8ce37b70499a30e402fc9dd2868611cc1c36b0
sanjeev <sanjeev@gmail.com>
2021-12-06 10:24:03 +0530

    App vax commit

---
+++ src/components/contactCompnent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class contactComponent extends React.Component{
+    render() {
+        return <h3>My App</h3>
+    }
+}
+export default contactComponent
---
+++ src/components/homeComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class homeComponent extends React.Component{
+    render() {
+        return <h3>Hi Well come to my app</h3>
+    }
+}
+export default homeComponent
